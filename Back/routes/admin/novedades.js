cc6c6abf0f4ab16ac49d55b604ad3346a42f76a5
Maxi Guillamon <guillamonmaxii@gmail.com>
2026-01-11 16:06:00 -0300

    commit

---
+++ Back/routes/admin/novedades.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET novedades admin page. */
+router.get('/', async function(req, res, next) {
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre
+    });
+
+});
+
+module.exports = router;
